8868a578f0c4c89a41c8abeb9f8c6b630b2e6078
Craig <indigomx9@gmail.com>
2022-02-27 01:45:53 -0500

    Added Update and Delete Routes and Controllers.

--- src/controllers/friendCon.ts
+++ src/controllers/friendCon.ts
@@ -30,6 +30,50 @@ class FriendsClass {
             next(error);
         }
     };
+
+    GetOne: express.RequestHandler =
+    async (req, res, next) => {
+        try {
+            const friend: Friends =
+            await Friends.findOneOrFail(req.params.id);
+            res.status(201).json(friend);
+        } catch (error: any) {
+            res.status(500).json({msg: error.message});
+            next(error);
+        }
+    };
+
+    Update: express.RequestHandler =
+    async (req, res, next) => {
+        try {
+            const friend: Friends =
+            await Friends.findOneOrFail(req.params.id);
+            friend.first = req.body.first;
+            friend.last = req.body.last;
+            friend.age = req.body.age;
+            friend.info = req.body.info;
+            friend.alignment = req.body.alignment;
+            await friend.save();
+            res.status(201).json(friend);
+        } catch (error: any) {
+            res.status(500).json({msg: error.message});
+            next(error);
+        }
+    };
+
+    Delete: express.RequestHandler =
+    async (req, res, next) => {
+        try {
+            const friend: Friends =
+            await Friends.findOneOrFail(req.params.id);
+            await friend.remove();
+            res.status(200)
+            .json("The Friend was Deleted!");
+        } catch (error: any) {
+            res.status(500).json({msg: error.message});
+            next(error);
+        }
+    };
 };
 
 export const FRIENDS: FriendsClass = new FriendsClass();
--- src/routes/friendRt.ts
+++ src/routes/friendRt.ts
@@ -4,6 +4,12 @@ import { FRIENDS } from "../controllers/friendCon";
 export const friendRt: express.Router = express.Router();
     friendRt.post("/", FRIENDS.Create);
     friendRt.get("/", FRIENDS.FetchAll);
+    friendRt.get("/:id", FRIENDS.GetOne);
+    friendRt.put("/:id", FRIENDS.Update);
+    friendRt.delete("/:id", FRIENDS.Delete);
 
 
 
+
+
+    
